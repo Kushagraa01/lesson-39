9ef05f81ff8537c66ef92f9be58eb100825402d9
Kushagraa01 <swatiabhijain@gmail.com>
2025-03-01 18:08:27 +0530

    lesson 39

---
+++ activity3.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number"))
+if num>50:
+    print("It is greater than 50")
+    if num%2==0:
+        print("and the number is even")
+    else:
+        print("and the number is odd")
+else:
+    print("It is smaller than 50")
